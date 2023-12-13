e5e74b91aea5b55ea32e5f16bcb0d917674cdce8
Brent Van Hese <brent.vanhese@icapps.com>
2023-12-13 14:49:15 +0100

    added dummy data

---
+++ types/threads.ts
@@ -0,0 +1,22 @@
+import { User } from "./User";
+
+export interface Thread {
+  id: string;
+  author: User;
+  content: string;
+  image?: string;
+  replies?: Reply[];
+  repliesCount: number;
+  likesCount: number;
+  mention?: boolean;
+  mentionUser?: User;
+  createdAt: string;
+}
+
+export interface Reply {
+  id: string;
+  author: User;
+  content: string;
+  likes: number;
+  createdAt: string;
+}
