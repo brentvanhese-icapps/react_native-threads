699b57e3da16d0768ff92f814e6d04ae9c857426
Brent Van Hese <brent.vanhese@icapps.com>
2023-12-14 10:12:25 +0100

    show replies; add reply

--- app/thread-details.tsx
+++ app/thread-details.tsx
@@ -1,18 +1,65 @@
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import ThreadsItem from "../components/ThreadsItem";
 import { useRoute } from "@react-navigation/native";
 import { useState } from "react";
-import { Thread } from "../types";
+import { Reply, Thread } from "../types";
+import ReplyItem from "../components/ReplyItem";
+import { Image } from "expo-image";
+import { createRandomFollower } from "../utils/generate-dummy-data";
 
 export default function ThreadDetails(): JSX.Element {
   const route = useRoute();
   const [thread, setThread] = useState(route.params as Thread);
+  const [replyContent, setReplyContent] = useState("");
+
+  function handleReply() {
+    const newReply: Reply = {
+      author: createRandomFollower(),
+      content: replyContent,
+      createdAt: new Date().toISOString(),
+      id: Math.random().toString(),
+      likes: 0,
+    };
+    setThread((prevState) => ({
+      ...prevState,
+      replies: [newReply, ...prevState.replies!],
+      repliesCount: prevState.repliesCount + 1,
+    }));
+    setReplyContent("");
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <ThreadsItem {...thread} />
+        <View style={styles.replies}>
+          {thread.replies?.map((reply) => (
+            <ReplyItem {...reply} key={reply.id} />
+          ))}
+        </View>
       </ScrollView>
+      <View style={styles.replyContainer}>
+        <Image
+          source={thread.author.photo}
+          style={styles.image}
+          contentFit="cover"
+          transition={200}
+        />
+        <TextInput
+          placeholder="Add reply"
+          value={replyContent}
+          onChangeText={setReplyContent}
+        />
+        <Button title="Reply" disabled={!replyContent} onPress={handleReply} />
+      </View>
     </SafeAreaView>
   );
 }
@@ -23,5 +70,25 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     padding: 10,
+  },
+  replies: {
+    gap: 15,
+  },
+  replyContainer: {
+    marginHorizontal: 30,
+    gap: 15,
+  },
+  image: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  input: {
+    flexGrow: 1,
+    backgroundColor: "#00000010",
+    marginLeft: 10,
+    padding: 10,
+    marginBottom: 30,
+    borderRadius: 10,
   },
 });
---
+++ components/ReplyItem.tsx
@@ -0,0 +1,49 @@
+import { Image } from "expo-image";
+import { Reply } from "../types";
+import { StyleSheet, View, Text } from "react-native";
+import { BottomIcons, PostFooter, PostHeading, blurhash } from "./ThreadsItem";
+
+export default function ReplyItem({
+  author,
+  content,
+  createdAt,
+  id,
+  likes,
+}: Reply): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={author.photo}
+        placeholder={blurhash}
+        contentFit="cover"
+        transition={200}
+      />
+      <View>
+        <PostHeading
+          name={author.name}
+          createdAt={createdAt}
+          verified={false}
+        />
+        <Text>{content}</Text>
+        <BottomIcons />
+        <PostFooter replies={0} likes={likes} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  authorPhoto: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  reply: {
+    flexShrink: 1,
+    gap: 10,
+  },
+});
